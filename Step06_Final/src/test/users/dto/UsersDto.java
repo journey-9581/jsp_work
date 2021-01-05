b09743824fc048a170dab0e7f3b3b4a8de6095ff
journey <tc02117@naver.com>
2021-01-05 18:32:59 +0900

    step 06 - pwd update form/pwd update added, dto, dao changed

--- Step06_Final/src/test/users/dto/UsersDto.java
+++ Step06_Final/src/test/users/dto/UsersDto.java
@@ -6,18 +6,20 @@ public class UsersDto {
 	private String email;
 	private String profile;
 	private String regdate;
+	private String newPwd;
 	
 	//디폴트 생성자
 	public UsersDto() {
 		
 	}
-
-	public UsersDto(String id, String pwd, String email, String profile, String regdate) {
+	
+	public UsersDto(String id, String pwd, String email, String profile, String regdate, String newPwd) {
 		this.id = id;
 		this.pwd = pwd;
 		this.email = email;
 		this.profile = profile;
 		this.regdate = regdate;
+		this.newPwd = newPwd;
 	}
 
 	public String getId() {
@@ -59,5 +61,13 @@ public class UsersDto {
 	public void setRegdate(String regdate) {
 		this.regdate = regdate;
 	}
-	
+
+	public String getNewPwd() {
+		return newPwd;
+	}
+
+	public void setNewPwd(String newPwd) {
+		this.newPwd = newPwd;
+	}
+
 }
